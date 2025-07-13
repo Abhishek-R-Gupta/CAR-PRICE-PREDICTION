90efeac42d094f6943947a23be69d38d41dc9738
Abhi Gupta <110951736+Abhishek-R-Gupta@users.noreply.github.com>
2025-07-13 13:01:52 +0530

    after cors

--- web/backend/app/main.py
+++ web/backend/app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
+# from streamlit.web.server import allow_cross_origin_requests
 
 app = FastAPI(title='Car Price Prediction API')
+
+origins =  ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_headers=["*"],
+    allow_methods=["*"]
+)
 
 app.include_router(router)
